3c21a782f9989d1135e6f3997b45c60f3a9b7254
DeivaSubramaniam <deivamanimca@gmail.com>
2017-12-19 11:56:41 +0530

    service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SiblingComponent } from './sibling/sibling.component';
+import { DataService } from './shared/data.service';
+import { Sibling1Component } from './sibling1/sibling1.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiblingComponent,
+    Sibling1Component
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
